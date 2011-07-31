519b5201feb01afd1affa11c2aca8b6431de9d6f
mo_kejp <mo_kejp@yahoo.co.jp>
2011-07-31 18:56:52 +0900

    タブスタイルの変更

--- src/org/sample/SampleContacts.java
+++ src/org/sample/SampleContacts.java
@@ -2,25 +2,67 @@ package org.sample;
 
 import android.app.TabActivity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
+import android.widget.TextView;
 
-public class SampleContacts extends TabActivity {
+public class SampleContacts extends TabActivity
+	implements OnTabChangeListener {
 
+	private TextView indexesTabText;
+	private TextView groupTabText;
 	@Override
     public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        indexesTabText = new TextView(this);
+        indexesTabText.setText(R.string.tab_indexes);
+        indexesTabText.setPadding(0, 5, 0, 0);
+        indexesTabText.setTextSize(15);
+        indexesTabText.setBackgroundColor(Color.LTGRAY);
+        indexesTabText.setTextColor(Color.WHITE);
+        indexesTabText.setGravity(Gravity.CENTER_HORIZONTAL);
+        indexesTabText.setHeight(39);
+        
+        
         TabHost.TabSpec indexes = 
-        	getTabHost().newTabSpec("indexes ").
-        		setIndicator(getString(R.string.tab_indexes)).setContent(new Intent(this, IndexContacts.class));  
+        	getTabHost().newTabSpec("indexes").
+        		setIndicator(indexesTabText).setContent(new Intent(this, IndexContacts.class));
+        
         getTabHost().addTab(indexes);         
 
+        
+        groupTabText = new TextView(this);
+        groupTabText.setText(R.string.tab_groups);
+        groupTabText.setPadding(0, 5, 0, 0);
+        groupTabText.setTextSize(15);
+        groupTabText.setBackgroundColor(Color.DKGRAY);
+        groupTabText.setTextColor(Color.WHITE);
+        groupTabText.setGravity(Gravity.CENTER_HORIZONTAL);
+        groupTabText.setHeight(39);
         TabHost.TabSpec group = 
         	getTabHost().newTabSpec("Group").
-        		setIndicator(getString(R.string.tab_groups)).setContent(new Intent(this, GroupContacts.class));  
+        		setIndicator(groupTabText).setContent(new Intent(this, GroupContacts.class));  
+        
         getTabHost().addTab(group);         
+        
+        getTabHost().setOnTabChangedListener(this);
 	}
+
+	@Override
+	public void onTabChanged(String tabId) {  
+		groupTabText.setBackgroundColor(Color.DKGRAY);  
+    	indexesTabText.setBackgroundColor(Color.DKGRAY); 
+	    if (tabId == "Group") {
+	    	groupTabText.setBackgroundColor(Color.LTGRAY);  
+	    } else if (tabId == "indexes"){  
+	    	indexesTabText.setBackgroundColor(Color.LTGRAY);  
+	    }
+	    
+	  } 
 }
